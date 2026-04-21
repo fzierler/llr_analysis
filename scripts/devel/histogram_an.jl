9688ae01d062c79b1e372b1d8df3ce9d4b4fa463
Fabian Zierler <fabian.zierler@tum.de>
2026-04-21 09:04:19 +0200

    all a_n histograms

--- scripts/devel/histogram_an.jl
+++ scripts/devel/histogram_an.jl
@@ -1,3 +1,5 @@
+using Pkg
+Pkg.activate(".")
 using LLRParsing
 using HDF5
 using Plots
@@ -14,16 +16,19 @@ gr(
     left_margin = 0Plots.mm,
 )
 
-file = "data_assets/Sp4_Nt5_sorted.hdf5"
+file = "data_assets/su4/all_su4_sorted.hdf5"
 h5id = h5open(file)
 runs = filter(startswith(r"[0-9]"), keys(h5id))
-r = runs[8]
+r = runs[5]
 N_replicas = read(h5id[r], "N_replicas")
 
-for replica in 1:N_replicas
+plts = []
+
+for replica in 0:(N_replicas - 1)
     an = last.(LLRParsing.a_trajectory(h5id, r; replica))
-    title = LLRParsing.fancy_title(r) * " - replica: $replica"
-    plt = plot(; title, xlabel = L"a_n", ylabel = "counts")
-    histogram!(plt, an, label = "")
-    display(plt)
+    plt = histogram(an, label = L"a_n", ylabel = "", ticks = :none, title = "replica #$replica")
+    push!(plts, plt)
 end
+
+pltF = plot(plts..., layout = grid(12, 8), size = (1000, 1500))
+savefig(pltF, "Histogram_an.pdf")
