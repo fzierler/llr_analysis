043e2d6e40fba5f9cb46144a654982c4e6ba25d2
Fabian Zierler <fabian.zierler@tum.de>
2026-04-21 08:00:30 +0200

    sort energy plot by central value

--- scripts/devel/energies.jl
+++ scripts/devel/energies.jl
@@ -1,3 +1,5 @@
+using Pkg
+Pkg.activate(".")
 using LLRParsing
 using HDF5
 using Plots
@@ -18,32 +20,50 @@ function read_therm_meas(h5id, name)
     llr_therm = read(h5id, "$name/0/Rep_0/llr_therm")
     llr_meas = read(h5id, "$name/0/Rep_0/llr_meas")
     ΔE = read(h5id, "$name/0/Rep_0/dS0")
+    E0 = read(h5id, "$name/0/Rep_0/S0")
 
     E_therm = zeros(llr_therm, N_replicas, N_repeats)
     E_meas = zeros(llr_meas, N_replicas, N_repeats)
+    E0 = zeros(N_replicas, N_repeats)
 
     for j in 0:(N_repeats - 1)
         for n in 0:(N_replicas - 1)
             E_meas[:, n + 1, j + 1] = read(h5id, "$name/$j/Rep_$n/E_meas")[:, end]
             E_therm[:, n + 1, j + 1] = read(h5id, "$name/$j/Rep_$n/E_therm")[:, end]
+            E0[n + 1, j + 1] = read(h5id, "$name/$j/Rep_$n/S0")[end]
         end
     end
 
-    return E_therm, E_meas, ΔE
+    return E_therm, E_meas, ΔE, E0
 end
 
-h5id = h5open("tmp/su4/su4.hdf5")
-name = "5x32_96replicas"
+function main()
 
-N_repeats = read(h5id, "$name/N_repeats")
-N_replicas = read(h5id, "$name/N_replicas")
-Nt = read(h5id, "$name/Nt")
-Ns = read(h5id, "$name/Ns")
+    h5id = h5open("tmp/su4/su4.hdf5")
+    name = "5x32_96replicas"
 
-E_therm, E_meas, ΔE = read_therm_meas(h5id, name)
+    N_repeats = read(h5id, "$name/N_repeats")
+    N_replicas = read(h5id, "$name/N_replicas")
+    Nt = read(h5id, "$name/Nt")
+    Ns = read(h5id, "$name/Ns")
 
-repeat = 0
-plts = [ histogram(E_meas[:, i, repeat + 1], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
-plt = plot(plts..., layout = grid(12, 8), size = (1000, 1500))
-plot!(plt, plot_title = LLRParsing.fancy_title(name))
-savefig("histograms.pdf")
+    E_therm, E_meas, ΔE, E0 = read_therm_meas(h5id, name)
+    for r in axes(E_therm, 3)
+        perm = sortperm(E0[:, r])
+        E_therm[:, :, r] = E_therm[:, perm, r]
+        E_meas[:, :, r] = E_meas[:, perm, r]
+    end
+
+    repeat = 1
+    replica = 50
+    plt1 = plot(E_meas[:, replica, repeat], label = "", title = "replica #$replica")
+    plts = [ histogram(E_meas[:, i, repeat + 1], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
+    plt2 = plot(plts..., layout = grid(12, 8), size = (1000, 1500))
+    plot!(plt2, plot_title = LLRParsing.fancy_title(name))
+
+    display(plt1)
+    return display(plt2)
+    #savefig("histograms.pdf")
+end
+
+main()
--- workflow/Snakefile_su4.smk
+++ workflow/Snakefile_su4.smk
@@ -18,13 +18,13 @@ rule all_su4:
         #    "assets/su4/plots/free_energy/free_energy_{dataset}.pdf",
         #    dataset=list_datasets(metadata),
         #),
-        an_volume_plots=expand("assets/su4/plots/an_volume_Nt{Nt}.pdf", Nt=[5, 6]),
+        an_volume_plots=expand("assets/su4/plots/an_volume_Nt{Nt}.pdf", Nt=[5, 6, 7, 8]),
         entropy_plots=expand("assets/su4/plots/entropy_Nt{Nt}.pdf", Nt=[5, 6]),
-        critical_beta_ratio_plots=expand(
-            "assets/su4/plots/critical_beta_ratios_Nt{Nt}.pdf", Nt=[5, 6]
-        ),
+        #critical_beta_ratio_plots=expand(
+        #    "assets/su4/plots/critical_beta_ratios_Nt{Nt}.pdf", Nt=[5, 6]
+        #),
         critical_beta_volume_plots=expand(
-            "assets/su4/plots/beta_critical_Nt{Nt}.pdf", Nt=[5, 6]
+            "assets/su4/plots/beta_critical_Nt{Nt}.pdf", Nt=[5, 6, 7, 8]
         ),
         double_gaussian_volume_plots=expand(
             "assets/su4/plots/plaquette_distribution_Nt{Nt}_volumes.pdf", Nt=[5, 6]
@@ -34,12 +34,15 @@ rule all_su4:
         #    dataset=list_datasets(metadata),
         #),
         plot_binder_cumulant=expand(
-            "assets/su4/plots/binder_cumulant_Nt{Nt}.pdf", Nt=[5, 6]
+            "assets/su4/plots/binder_cumulant_Nt{Nt}.pdf", Nt=[5, 6, 7, 8]
         ),
         plot_specific_heat=expand(
-            "assets/su4/plots/specific_heat_Nt{Nt}.pdf", Nt=[5, 6]
+            "assets/su4/plots/specific_heat_Nt{Nt}.pdf", Nt=[5, 6, 7, 8]
         ),
         #surface_tension_plot="assets/su4/plots/surface_tension_term.pdf",
         #ensembles_combined="assets/su4/tables/runs.tex",
         #file_out="assets/su4/tables/beta_critical.tex",
         supercooling_csv="data_assets/su4/supercooling_param.csv",
+        an_replicas=expand(
+            "assets/su4/plots/an_replicas_Nt{Nt}_Ns{Ns}.pdf", zip, Nt=[5], Ns=[28]
+        ),
