185e8dfeb93b941a3c46b14a04872bf24087406f
Fabian Zierler <fabian.zierler@a1.net>
2026-05-01 12:49:47 +0200

    more consistent plotting of polyakov loop data

--- scripts/devel/polyakov_loop.jl
+++ scripts/devel/polyakov_loop.jl
@@ -28,12 +28,10 @@ function logZ_fxa(S_fxa, S0, an, β)
     # figure out sorting of S0_fxa
     return VEV_exp = @. (-an[1] + β) * S_fxa[1] # +an[1]*S0[1] + logρ - log(length(S0_fxa[1])) + log(dS)
 end
-function read_sort_poly_data(h5,ens,repeat)
+function read_sort_poly_data(h5,ens,repeat,nfxa_swap,nfxa_meas)
     # The following quantities are not reliably logged in the output files
     # But we can deduce them from the total number of measurements
     Nrep = read(h5[ens], "N_replicas")
-    nfxa_swap = length(h5["$ens/$repeat/Rep_0/S0_fxa"])
-    nfxa_meas = length(h5["$ens/$repeat/Rep_0/poly"])÷nfxa_swap
     S0_fxa = zeros(Nrep, nfxa_swap)
     an_fxa = zeros(Nrep, nfxa_swap)
     poly_fxa = zeros(ComplexF64, (Nrep, nfxa_meas, nfxa_swap))
@@ -58,8 +56,6 @@ function read_sort_poly_data(h5,ens,repeat)
     S0 = S0_fxa_sorted[:,1]
     an = an_fxa_sorted[:,1]
     poly = reshape(poly_fxa_sorted,(Nrep,nfxa_meas*nfxa_swap))
-    # for Z2 symmetric theories the polyakov loop is real
-    poly = real.(poly)
     return an, S0, poly
 end
 function plot_poly_overview(an,up,poly,plotpath,plotname,repeat,ens;f1,f2)
@@ -73,7 +69,7 @@ function plot_poly_overview(an,up,poly,plotpath,plotname,repeat,ens;f1,f2)
 
     kws = (bins=:sqrt,normalize=:probability,xlabel=L"\ell_p",title=title)
     plts = [histogram(poly[i,1:f1:end],label="replica #$i"; kws...) for i in 1:Nrep]
-    @showprogress for (i,p) in enumerate(plts)
+    @showprogress desc=ens for (i,p) in enumerate(plts)
         b0 = LLRParsing._highlight_replica!(deepcopy(plt),up,i; color = :green, alpha = 0.5, labels = "replica")
         a0 = LLRParsing._highlight_replica!(deepcopy(plt_an),up,i; color = :green, alpha = 0.5, labels = "replica")
         p0 = plot(p, b0, a0, layout = grid(3, 1), size = (425, 846))
@@ -84,6 +80,7 @@ function plot_poly_overview(an,up,poly,plotpath,plotname,repeat,ens;f1,f2)
     end
 end
 
+h5file = "tmp/su3/su3.hdf5"
 h5file = "tmp/sp4/sp4.hdf5"
 h5 = h5open(h5file)
 for ens in keys(h5)
@@ -91,15 +88,27 @@ for ens in keys(h5)
     Nrep = read(h5[ens], "N_replicas")
     Nt = read(h5[ens],"Nt")
     Ns = read(h5[ens],"Ns")
-    V = Nt*Ns^3
 
-    an, S0, poly = read_sort_poly_data(h5,ens,first(repeats))
-    up = S0/(6V)
+    # skip for now if plot exists 
+    plotpath = "."
+    plotname = "polyakov_loop_su3_$(ens)_scatter.pdf"
+
+    # obtain fixed-a parameters
+    nfxa_swap = length(h5["$ens/$(first(repeats))/Rep_0/S0_fxa"])
+    npoly_meas = length(h5["$ens/$(first(repeats))/Rep_0/poly"])
+    # if we haven't performed any such measurement we skip the ensemble
+    iszero(nfxa_swap) && continue
+    nfxa_meas = npoly_meas÷nfxa_swap
+
+    an, S0, poly = read_sort_poly_data(h5,ens,first(repeats),nfxa_swap,nfxa_meas)
+    up = S0/(6Nt*Ns^3)
+
+    # for Z2 symmetric theories the polyakov loop is real
+    # for ZN symmetric theories the polyakov loop is complex
+    poly = real.(poly)
 
     # frequency of plotting  (for smaller files)
     f1 = 20
-    f2 = 100
-    plotpath = "."
-    plotname = "polyakov_loop_$ens.pdf"
+    f2 = nfxa_meas÷100
     plot_poly_overview(an,up,poly,plotpath,plotname,first(repeats),ens;f1,f2)
 end
