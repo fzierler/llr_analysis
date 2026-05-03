3e04cc0570ff6dba99cfc377486ff4860fc9be56
Fabian Zierler <fabian.zierler@a1.net>
2026-05-03 17:03:35 +0200

    add labels

--- scripts/devel/polyakov_loop.jl
+++ scripts/devel/polyakov_loop.jl
@@ -20,14 +20,15 @@ function logZ_fxa(S_fxa, S0, an, β)
     return VEV_exp = @. (-an[1] + β) * S_fxa[1] # +an[1]*S0[1] + logρ - log(length(S0_fxa[1])) + log(dS)
 end
 
-h5file = "data_assets/sp4/all_sp4_sorted.hdf5"
 h5file = "data_assets/su3/all_su3_sorted.hdf5"
+h5file = "data_assets/sp4/all_sp4_sorted.hdf5"
 
 h5 = h5open(h5file)
 for ens in filter(!isequal("provenance"),keys(h5))    
     # lattice volume
     Nt = read(h5[ens],"Nt")
     Ns = read(h5[ens],"Ns")
+    Nint = read(h5[ens],"N_replicas")
     repeats = read(h5[ens],"repeats")
     group = read(h5[ens],"group")
     # obtain fixed-a parameters
@@ -52,19 +53,22 @@ for ens in filter(!isequal("provenance"),keys(h5))
     points3D = StructArray{Point3f}((vec(up), vec(poly_im), vec(poly_abs)))
 
     # TODO:
-    # 1) Axis labels and title
     # 2) Grid for energy intervals
     # 3) Add scatter plot/histogram of fixed energy 
     # 4) Add corresponding hightlight to shader plot
     # 5) Add a plot of an vs. up 
+    # 6) Average over repeats, and increase dpi
 
     fig = Figure()
+    title = L"%$Nt\times%$(Ns)^3,~N_{\mathrm{rep}}=%$Nint"
+    xlabel = L"u_p"
     set_theme!(theme_latexfonts())
-    ax = Axis(fig[1, 1], title = "Title")
+
     if group == "SU(3)"
-        #datashader!(ax,points_im,colormap=[:transparent, :grey, :black])
+        ax = Axis(fig[1, 1]; title, xlabel, ylabel = L"\text{Im}(\ell_p)")
         datashader!(ax,points3D,agg = Makie.AggMean(), operation = identity)
     else
+        ax = Axis(fig[1, 1]; title, xlabel, ylabel = L"\text{Im}(\ell_p)")
         datashader!(ax,points_re,colormap=[:transparent, :grey, :black])
     end
     save("$(group)_$ens.pdf",fig)
