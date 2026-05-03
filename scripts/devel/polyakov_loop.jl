c75e52a469639636324b4195e26265b0ce49e57e
Fabian Zierler <fabian.zierler@a1.net>
2026-05-03 16:36:13 +0200

    automatically choose good visual representation for a given gauge group

--- scripts/devel/polyakov_loop.jl
+++ scripts/devel/polyakov_loop.jl
@@ -20,14 +20,16 @@ function logZ_fxa(S_fxa, S0, an, β)
     return VEV_exp = @. (-an[1] + β) * S_fxa[1] # +an[1]*S0[1] + logρ - log(length(S0_fxa[1])) + log(dS)
 end
 
-h5file = "data_assets/su3/all_su3_sorted.hdf5"
 h5file = "data_assets/sp4/all_sp4_sorted.hdf5"
+h5file = "data_assets/su3/all_su3_sorted.hdf5"
+
 h5 = h5open(h5file)
 for ens in filter(!isequal("provenance"),keys(h5))    
     # lattice volume
     Nt = read(h5[ens],"Nt")
     Ns = read(h5[ens],"Ns")
     repeats = read(h5[ens],"repeats")
+    group = read(h5[ens],"group")
     # obtain fixed-a parameters
     an = read(h5["$ens/$(first(repeats))"],"an_fxa")
     S0 = read(h5["$ens/$(first(repeats))"],"S0_fxa")
@@ -45,11 +47,25 @@ for ens in filter(!isequal("provenance"),keys(h5))
     up = E/(6Nt*Ns^3)
 
     # set up points for plotting 
-    points = StructArray{Point2f}((vec(up), vec(poly_re)))
+    points_re = StructArray{Point2f}((vec(up), vec(poly_re)))
+    points_im = StructArray{Point2f}((vec(up), vec(poly_im)))
+    points3D = StructArray{Point3f}((vec(up), vec(poly_im), vec(poly_abs)))
+
+    # TODO:
+    # 1) Axis labels and title
+    # 2) Grid for energy intervals
+    # 3) Add scatter plot/histogram of fixed energy 
+    # 4) Add corresponding hightlight to shader plot
+    # 5) Add a plot of an vs. up 
 
     fig = Figure()
     set_theme!(theme_latexfonts())
     ax = Axis(fig[1, 1], title = "Title")
-    datashader!(ax,points,colormap=[:transparent, :grey, :black])
-    save("$ens.pdf",fig)
+    if group == "SU(3)"
+        #datashader!(ax,points_im,colormap=[:transparent, :grey, :black])
+        datashader!(ax,points3D,agg = Makie.AggMean(), operation = identity)
+    else
+        datashader!(ax,points_re,colormap=[:transparent, :grey, :black])
+    end
+    save("$(group)_$ens.pdf",fig)
 end
