ff195147d66906883c453eb3c4dce70534373c37
Fabian Zierler <fabian.zierler@tum.de>
2026-06-25 21:40:41 +0200

    only collect data for specific repeat

--- scripts/energy_histogram.jl
+++ scripts/energy_histogram.jl
@@ -13,33 +13,27 @@ gr(
     left_margin = 0Plots.mm,
     top_margin = 0Plots.mm,
 )
-function read_therm_meas(h5id, name)
-    repeats = read(h5id, "$name/repeats")
-    N_repeats = length(repeats)
+function read_therm_meas(h5id, name,repeat)
     N_replicas = read(h5id, "$name/N_replicas")
-    llr_therm = read(h5id, "$name/0/Rep_0/llr_therm")
-    llr_meas = read(h5id, "$name/0/Rep_0/llr_meas")
-    ΔE = read(h5id, "$name/0/Rep_0/dS0")
-    E0 = read(h5id, "$name/0/Rep_0/S0")
+    llr_therm = read(h5id, "$name/$repeat/Rep_0/llr_therm")
+    llr_meas = read(h5id, "$name/$repeat/Rep_0/llr_meas")
+    ΔE = read(h5id, "$name/$repeat/Rep_0/dS0")
+    E0 = read(h5id, "$name/$repeat/Rep_0/S0")
 
-    E_therm = zeros(llr_therm, N_replicas, N_repeats)
-    E_meas = zeros(llr_meas, N_replicas, N_repeats)
-    E0 = zeros(N_replicas, N_repeats)
+    E_therm = zeros(llr_therm, N_replicas)
+    E_meas = zeros(llr_meas, N_replicas)
+    E0 = zeros(N_replicas)
 
-    for (j,repeat) in enumerate(repeats)
-        for n in 0:(N_replicas - 1)
-            E_meas[:, n + 1, j] = read(h5id, "$name/$repeat/Rep_$n/E_meas")[:, end]
-            E_therm[:, n + 1, j] = read(h5id, "$name/$repeat/Rep_$n/E_therm")[:, end]
-            E0[n + 1, j] = read(h5id, "$name/$repeat/Rep_$n/S0")[end]
-        end
+    for n in 0:(N_replicas - 1)
+        E_meas[:, n + 1] = read(h5id, "$name/$repeat/Rep_$n/E_meas")[:, end]
+        E_therm[:, n + 1] = read(h5id, "$name/$repeat/Rep_$n/E_therm")[:, end]
+        E0[n + 1] = read(h5id, "$name/$repeat/Rep_$n/S0")[end]
     end
 
     # sort by central energy
-    for r in axes(E_therm, 3)
-        perm = sortperm(E0[:, r])
-        E_therm[:, :, r] = E_therm[:, perm, r]
-        E_meas[:, :, r] = E_meas[:, perm, r]
-    end
+    perm    = sortperm(E0)
+    E_therm = E_therm[:, perm]
+    E_meas  = E_meas[:, perm]
 
     return E_therm, E_meas, ΔE, E0
 end
@@ -50,11 +44,11 @@ function main(file, name, dir;repeat = 1)
     repeats = read(h5id, "$name/repeats")
     Nrep = read(h5id, "$name/N_replicas")
     
-    has_therm = all( [ haskey(h5id, "$name/$r/Rep_$n/E_therm") for r in repeats, n in 0:Nrep-1])
-    has_meas  = all( [ haskey(h5id, "$name/$r/Rep_$n/E_meas") for r in repeats, n in 0:Nrep-1])
+    has_therm = all( [ haskey(h5id, "$name/$repeat/Rep_$n/E_therm") for n in 0:Nrep-1])
+    has_meas  = all( [ haskey(h5id, "$name/$repeat/Rep_$n/E_meas")  for n in 0:Nrep-1])
     
     if has_therm && has_meas
-        E_therm, E_meas, ΔE, E0 = read_therm_meas(h5id, name)
+        E_therm, E_meas, ΔE, E0 = read_therm_meas(h5id, name, repeat)
 
         E_full = cat(E_therm,E_meas,dims=1)
         therms = size(E_therm,1) 
@@ -63,11 +57,11 @@ function main(file, name, dir;repeat = 1)
         cols = 12
         rows = cld(nreplicas,cols) 
         
-        plts = [ plot(E_full[:, i, repeat], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
+        plts = [ plot(E_full[:, i], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
         plts = [ vspan!(plt, [1, therms], color = :blue, alpha = 0.2, labels = "therm") for plt in plts ]
         plt1 = plot(plts..., layout = grid(cols, rows), size = (1000, 1500))
         
-        plts = [ histogram(E_meas[:, i, repeat + 1], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
+        plts = [ histogram(E_meas[:, i], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
         plt2 = plot(plts..., layout = grid(cols, rows), size = (1000, 1500))
         plot!(plt2, plot_title = LLRParsing.fancy_title(name))
 
