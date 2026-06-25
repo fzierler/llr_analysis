56e65d6054fe572b19007ac349eb9a3489e75b1c
Fabian Zierler <fabian.zierler@tum.de>
2026-06-25 21:17:54 +0200

    make plot for all datasets for which we have data on the energy

--- scripts/energy_histogram.jl
+++ scripts/energy_histogram.jl
@@ -14,7 +14,8 @@ gr(
     top_margin = 0Plots.mm,
 )
 function read_therm_meas(h5id, name)
-    N_repeats = read(h5id, "$name/N_repeats")
+    repeats = read(h5id, "$name/repeats")
+    N_repeats = length(repeats)
     N_replicas = read(h5id, "$name/N_replicas")
     llr_therm = read(h5id, "$name/0/Rep_0/llr_therm")
     llr_meas = read(h5id, "$name/0/Rep_0/llr_meas")
@@ -25,11 +26,11 @@ function read_therm_meas(h5id, name)
     E_meas = zeros(llr_meas, N_replicas, N_repeats)
     E0 = zeros(N_replicas, N_repeats)
 
-    for j in 0:(N_repeats - 1)
+    for (j,repeat) in enumerate(repeats)
         for n in 0:(N_replicas - 1)
-            E_meas[:, n + 1, j + 1] = read(h5id, "$name/$j/Rep_$n/E_meas")[:, end]
-            E_therm[:, n + 1, j + 1] = read(h5id, "$name/$j/Rep_$n/E_therm")[:, end]
-            E0[n + 1, j + 1] = read(h5id, "$name/$j/Rep_$n/S0")[end]
+            E_meas[:, n + 1, j] = read(h5id, "$name/$repeat/Rep_$n/E_meas")[:, end]
+            E_therm[:, n + 1, j] = read(h5id, "$name/$repeat/Rep_$n/E_therm")[:, end]
+            E0[n + 1, j] = read(h5id, "$name/$repeat/Rep_$n/S0")[end]
         end
     end
 
@@ -45,21 +46,37 @@ end
 
 function main(file, name, dir;repeat = 1)
     h5id = h5open(file)
-    E_therm, E_meas, ΔE, E0 = read_therm_meas(h5id, name)
 
-    E_full = cat(E_therm,E_meas,dims=1)
-    therms = size(E_therm,1) 
+    repeats = read(h5id, "$name/repeats")
+    Nrep = read(h5id, "$name/N_replicas")
     
-    plts = [ plot(E_full[:, i, repeat], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
-    plts = [ vspan!(plt, [1, therms], color = :blue, alpha = 0.2, labels = "therm") for plt in plts ]
-    plt1 = plot(plts..., layout = grid(12, 8), size = (1000, 1500))
+    has_therm = all( [ haskey(h5id, "$name/$r/Rep_$n/E_therm") for r in repeats, n in 0:Nrep-1])
+    has_meas  = all( [ haskey(h5id, "$name/$r/Rep_$n/E_meas") for r in repeats, n in 0:Nrep-1])
     
-    plts = [ histogram(E_meas[:, i, repeat + 1], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
-    plt2 = plot(plts..., layout = grid(12, 8), size = (1000, 1500))
-    plot!(plt2, plot_title = LLRParsing.fancy_title(name))
+    if has_therm && has_meas
+        E_therm, E_meas, ΔE, E0 = read_therm_meas(h5id, name)
 
-    savefig(plt1, joinpath(dir,"energy_trajectory_$name.pdf"))
-    savefig(plt2, joinpath(dir,"energy_histogram_$name.pdf"))
+        E_full = cat(E_therm,E_meas,dims=1)
+        therms = size(E_therm,1) 
+
+        nreplicas = size(E_meas, 2)
+        cols = 12
+        rows = cld(nreplicas,cols) 
+        
+        plts = [ plot(E_full[:, i, repeat], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
+        plts = [ vspan!(plt, [1, therms], color = :blue, alpha = 0.2, labels = "therm") for plt in plts ]
+        plt1 = plot(plts..., layout = grid(cols, rows), size = (1000, 1500))
+        
+        plts = [ histogram(E_meas[:, i, repeat + 1], ticks = :none, label = "", title = "replica #$i") for i in axes(E_meas, 2) ]
+        plt2 = plot(plts..., layout = grid(cols, rows), size = (1000, 1500))
+        plot!(plt2, plot_title = LLRParsing.fancy_title(name))
+
+        savefig(plt1, joinpath(dir,"energy_trajectory_$name.pdf"))
+        savefig(plt2, joinpath(dir,"energy_histogram_$name.pdf"))
+    else
+        savefig(plot(), joinpath(dir,"energy_trajectory_$name.pdf"))
+        savefig(plot(), joinpath(dir,"energy_histogram_$name.pdf"))
+    end
     return nothing
 end
 
--- workflow/Snakefile_su4.smk
+++ workflow/Snakefile_su4.smk
@@ -46,6 +46,9 @@ rule all_su4:
         an_replicas=expand(
             "assets/su4/plots/an_replicas_Nt{Nt}_Ns{Ns}.pdf", zip, Nt=[5], Ns=[28]
         ),
-        plot_traj="assets/su4/plots/energies/energy_trajectory_5x32_96replicas.pdf",
-        plot_hist="assets/su4/plots/energies/energy_histogram_5x32_96replicas.pdf",
+        plot_traj_hist=expand(
+            "assets/su4/plots/energies/energy_{plot}_{dataset}.pdf",
+            dataset=list_datasets(metadata),
+            plot=("trajectory","histogram"),
+        ),
 
