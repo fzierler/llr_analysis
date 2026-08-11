8b1fc8b744c54dd9857427e71f9b8a0a20e96d54
Fabian Zierler <fabian.zierler@tum.de>
2026-08-11 15:19:10 +0200

    create combined info file

--- scripts/devel/collect_info.jl
+++ scripts/devel/collect_info.jl
@@ -3,20 +3,40 @@ Pkg.activate(".")
 using LLRParsing
 using DelimitedFiles
 
-function copy_info_files(metadata_file)
+function copy_info_files(metadata_file,path,file)
 
     metadata = readdlm(metadata_file, ',', String, skipstart = 1)
+    io_combined =  open(file,"w")
 
     for relpath in metadata[:,1]
-
         infofile = joinpath(relpath,"base","info.csv")
-
-        path = "tmp/su4/info"
         name = basename(relpath)
         ispath(joinpath(path)) || mkpath(joinpath(path))
+        # copy to new path
         cp(infofile,joinpath(path,"$(name)_info.csv"))
+        # create a combined file with info on all runs
+        for l in eachline(infofile)
+            startswith(l,'#') || println(io_combined,l)
+        end
     end
+    close(io_combined)
+    # now remove duplicates to only have a single header 
+    lines = unique(readlines(file))
+    io_combined =  open(file,"w")
+    for l in lines
+        println(io_combined,l)
+    end
+    close(io_combined)
+    # now sort the data by volume and replicas
+    data, header = readdlm(file,',',header=true)
+    data = sortslices(data; dims = 1, by = row -> (row[2],row[3],row[4]))
+    io_combined =  open(file,"w")
+    writedlm(io_combined,header,',')
+    writedlm(io_combined,data,',')
+    close(io_combined)
 end
 
+path = "tmp/su4/info"
+file = "tmp/su4/info_combined.csv"
 metadata_file = "metadata/runs_su4.csv"
-copy_info_files(metadata_file)
+copy_info_files(metadata_file,path,file)
